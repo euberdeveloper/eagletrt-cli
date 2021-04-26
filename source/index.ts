e2447b3735acfa4ac46e9b5ebad25d79a80e83d0
euberdeveloper <euberdeveloper@gmail.com>
2021-04-27 00:01:16 +0200

    feat: publish first version
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -2,6 +2,8 @@
 import * as yargs from 'yargs';
 
 import * as postProcessingCommands from '@eagletrt/telemetria-postprocessing/bundled/bin/commands';
+import * as codeGeneratorCommands from '@eagletrt/code-generator/bundled/bin/commands';
+import * as simulatorCommands from '@eagletrt/telemetria-simulator/bundled/bin/commands';
 
 yargs
     .scriptName('eagle')
@@ -37,9 +39,80 @@ yargs
                     postProcessingCommands.testCommand.handler(args);
                 }
             )
-            .demandCommand(1, 'You must specify a command for process');
+            .demandCommand(1, 'You must specify a command');
     })
+    .command(
+        'transpile',
+        codeGeneratorCommands.generateCommand.description,
+        yargs => {
+            yargs.options(codeGeneratorCommands.generateCommand.options);
+        },
+        args => {
+            codeGeneratorCommands.generateCommand.handler(args);
+        }
+    )
+    .command('virtualize', simulatorCommands.virtualizeCommand.description, yargs => {
+        yargs
+            .command(
+                simulatorCommands.virtualizeCommand.canCommand.command,
+                simulatorCommands.virtualizeCommand.canCommand.description,
+                yargs => {
+                    yargs.options(simulatorCommands.virtualizeCommand.canCommand.options);
+                },
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                async args => {
+                    await simulatorCommands.virtualizeCommand.canCommand.handler(args);
+                }
+            )
+            .demandCommand(1, 'You must specify a command');
+    })
+    .command('simulate', simulatorCommands.simulateCommand.description, yargs => {
+        yargs
+            .command(
+                simulatorCommands.simulateCommand.canCommand.command,
+                simulatorCommands.simulateCommand.canCommand.description,
+                yargs => {
+                    yargs.options(simulatorCommands.simulateCommand.canCommand.options);
+                },
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                async args => {
+                    await simulatorCommands.simulateCommand.canCommand.handler(args);
+                }
+            )
+            .command(
+                simulatorCommands.simulateCommand.gpsCommand.command,
+                simulatorCommands.simulateCommand.gpsCommand.description,
+                yargs => {
+                    yargs.options(simulatorCommands.simulateCommand.gpsCommand.options);
+                },
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                async args => {
+                    await simulatorCommands.simulateCommand.gpsCommand.handler(args);
+                }
+            )
+            .command(
+                simulatorCommands.simulateCommand.allCommand.command,
+                simulatorCommands.simulateCommand.allCommand.description,
+                yargs => {
+                    yargs.options(simulatorCommands.simulateCommand.allCommand.options);
+                },
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                async args => {
+                    await simulatorCommands.simulateCommand.allCommand.handler(args);
+                }
+            )
+            .demandCommand(1, 'You must specify a command');
+    })
+    .command(
+        'settings',
+        simulatorCommands.settingsCommand.description,
+        yargs => {
+            yargs.options(simulatorCommands.settingsCommand.options);
+        },
+        args => {
+            simulatorCommands.settingsCommand.handler(args) as unknown;
+        }
+    )
     .demandCommand(1, 'You must specify a command')
-    .epilogue(
-        'For more information, find our manual at https://github.com/euberdeveloper/eagletrt-code-generator#readme'
-    ).argv;
+    .strict()
+    .epilogue('For more information, find our manual at https://github.com/euberdeveloper/eagle-cli#readme').argv;
