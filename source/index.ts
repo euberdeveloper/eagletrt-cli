8570dfbc59fac93a24eaf003b7c2b999200393ee
euberdeveloper <euberdeveloper@gmail.com>
2021-04-26 21:51:50 +0200

    fix: fix skeleton project
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -1,47 +1,45 @@
 #!/usr/bin/env node
 import * as yargs from 'yargs';
 
-import * as postProcessingCommands from '@eagletrt/telemetria-postprocessing/bundled/bin';
+import * as postProcessingCommands from '@eagletrt/telemetria-postprocessing/bundled/bin/commands';
 
-if (require.main === module) {
-    yargs
-        .scriptName('eagle')
-        .command('process', 'Post process can or gps log files', yargs => {
-            yargs
-                .command(
-                    postProcessingCommands.csvCommand.command,
-                    postProcessingCommands.csvCommand.description,
-                    yargs => {
-                        yargs.options(postProcessingCommands.csvCommand.options);
-                    },
-                    args => {
-                        postProcessingCommands.csvCommand.handler(args);
-                    }
-                )
-                .command(
-                    postProcessingCommands.jsonCommand.command,
-                    postProcessingCommands.jsonCommand.description,
-                    yargs => {
-                        yargs.options(postProcessingCommands.jsonCommand.options);
-                    },
-                    args => {
-                        postProcessingCommands.jsonCommand.handler(args);
-                    }
-                )
-                .command(
-                    postProcessingCommands.testCommand.command,
-                    postProcessingCommands.testCommand.description,
-                    yargs => {
-                        yargs.options(postProcessingCommands.testCommand.options);
-                    },
-                    args => {
-                        postProcessingCommands.testCommand.handler(args);
-                    }
-                )
-                .demandCommand(1, 'You must specify a command for process');
-        })
-        .demandCommand(1, 'You must specify a command')
-        .epilogue(
-            'For more information, find our manual at https://github.com/euberdeveloper/eagletrt-code-generator#readme'
-        ).argv;
-}
+yargs
+    .scriptName('eagle')
+    .command('process', 'Post process can or gps log files', yargs => {
+        yargs
+            .command(
+                postProcessingCommands.csvCommand.command,
+                postProcessingCommands.csvCommand.description,
+                yargs => {
+                    yargs.options(postProcessingCommands.csvCommand.options);
+                },
+                args => {
+                    postProcessingCommands.csvCommand.handler(args);
+                }
+            )
+            .command(
+                postProcessingCommands.jsonCommand.command,
+                postProcessingCommands.jsonCommand.description,
+                yargs => {
+                    yargs.options(postProcessingCommands.jsonCommand.options);
+                },
+                args => {
+                    postProcessingCommands.jsonCommand.handler(args);
+                }
+            )
+            .command(
+                postProcessingCommands.testCommand.command,
+                postProcessingCommands.testCommand.description,
+                yargs => {
+                    yargs.options(postProcessingCommands.testCommand.options);
+                },
+                args => {
+                    postProcessingCommands.testCommand.handler(args);
+                }
+            )
+            .demandCommand(1, 'You must specify a command for process');
+    })
+    .demandCommand(1, 'You must specify a command')
+    .epilogue(
+        'For more information, find our manual at https://github.com/euberdeveloper/eagletrt-code-generator#readme'
+    ).argv;
